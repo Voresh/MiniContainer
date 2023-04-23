3acef6257c644363ba0cf77a4e19b157502e157f
Vladimir Oreshkov <voresh95@gmail.com>
2023-04-23 14:22:23 +0200

    zenject, vcontainer performance comparison

--- Performance/Assets/Tests/Performance/ContainerPerformanceTests.cs
+++ Performance/Assets/Tests/Performance/ContainerPerformanceTests.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using Unity.PerformanceTesting;
 using MiniContainer.InstanceConstructors;
+using VContainer;
+using Zenject;
 
 namespace MiniContainer.Performance.Tests.Runtime
 {
@@ -40,7 +42,6 @@ namespace MiniContainer.Performance.Tests.Runtime
                     }
                 })
                 .SampleGroup("Reflection")
-                .GC()
                 .Run();
             container.SetInstanceConstructors(new MiniContainerTestsPerformance_GeneratedInstanceConstructor());
             Measure.Method(() =>
@@ -50,8 +51,30 @@ namespace MiniContainer.Performance.Tests.Runtime
                         container.Resolve<ClassA>();
                     }
                 })
-                .SampleGroup("CodeGen")
-                .GC()
+                .SampleGroup("MiniContainer")
+                .Run();
+            var zenjectContainer = new DiContainer();
+            zenjectContainer.Bind<ClassA>().AsTransient().Lazy();
+            Measure.Method(() =>
+                {
+                    for (var i = 0; i < 10000; i++)
+                    {
+                        zenjectContainer.Resolve<ClassA>();
+                    }
+                })
+                .SampleGroup("Zenject")
+                .Run();
+            var builder = new ContainerBuilder();
+            builder.Register<ClassA>(Lifetime.Transient);
+            var vContainer = builder.Build();
+            Measure.Method(() =>
+                {
+                    for (var i = 0; i < 10000; i++)
+                    {
+                        vContainer.Resolve<ClassA>();
+                    }
+                })
+                .SampleGroup("VContainer")
                 .Run();
         }
 
@@ -72,7 +95,6 @@ namespace MiniContainer.Performance.Tests.Runtime
                     }
                 })
                 .SampleGroup("Reflection")
-                .GC()
                 .Run();
             container.SetInstanceConstructors(new MiniContainerTestsPerformance_GeneratedInstanceConstructor());
             Measure.Method(() =>
@@ -82,7 +104,74 @@ namespace MiniContainer.Performance.Tests.Runtime
                         container.Resolve<ClassB>();
                     }
                 })
-                .SampleGroup("CodeGen")
+                .SampleGroup("MiniContainer")
+                .Run();
+            var zenjectContainer = new DiContainer();
+            zenjectContainer.Bind<ClassA>().AsTransient().Lazy();
+            zenjectContainer.Bind<ClassB>().AsTransient().Lazy();
+            zenjectContainer.Bind<ClassC>().AsTransient().Lazy();
+            zenjectContainer.Bind<ClassD>().AsTransient().Lazy();
+            Measure.Method(() =>
+                {
+                    for (var i = 0; i < 10000; i++)
+                    {
+                        zenjectContainer.Resolve<ClassB>();
+                    }
+                })
+                .SampleGroup("Zenject")
+                .Run();
+            var builder = new ContainerBuilder();
+            builder.Register<ClassA>(Lifetime.Transient);
+            builder.Register<ClassB>(Lifetime.Transient);
+            builder.Register<ClassC>(Lifetime.Transient);
+            builder.Register<ClassD>(Lifetime.Transient);
+            var vContainer = builder.Build();
+            Measure.Method(() =>
+                {
+                    for (var i = 0; i < 10000; i++)
+                    {
+                        vContainer.Resolve<ClassB>();
+                    }
+                })
+                .SampleGroup("VContainer")
+                .Run();
+        }
+
+        [Test, Performance]
+        public void RegisterComplexTest()
+        {
+            Measure.Method(() =>
+                {
+                    var container = new Container();
+                    container.Register<ClassA>(false);
+                    container.Register<ClassB>(false);
+                    container.Register<ClassC>(false);
+                    container.Register<ClassD>(false);
+                })
+                .SampleGroup("MiniContainer")
+                .GC()
+                .Run();
+            Measure.Method(() =>
+                {
+                    var zenjectContainer = new DiContainer();
+                    zenjectContainer.Bind<ClassA>().AsTransient().Lazy();
+                    zenjectContainer.Bind<ClassB>().AsTransient().Lazy();
+                    zenjectContainer.Bind<ClassC>().AsTransient().Lazy();
+                    zenjectContainer.Bind<ClassD>().AsTransient().Lazy();
+                })
+                .SampleGroup("Zenject")
+                .GC()
+                .Run();
+            Measure.Method(() =>
+                {
+                    var builder = new ContainerBuilder();
+                    builder.Register<ClassA>(Lifetime.Transient);
+                    builder.Register<ClassB>(Lifetime.Transient);
+                    builder.Register<ClassC>(Lifetime.Transient);
+                    builder.Register<ClassD>(Lifetime.Transient);
+                    builder.Build();
+                })
+                .SampleGroup("VContainer")
                 .GC()
                 .Run();
         }
